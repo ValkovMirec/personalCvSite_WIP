35930f77360f4022e9b69bb15529bb1ea7d4d2a1
Miroslav Valkovič <valkovic1993@gmail.com>
2020-10-29 19:37:25 +0100

    Responsive design
    Finished on all devices. Next step, hamburger menu.

--- src/blocks/HeaderBlock.js
+++ src/blocks/HeaderBlock.js
@@ -1,16 +1,12 @@
 import React, { useEffect, useState } from "react";
 
+import { TiThMenuOutline } from 'react-icons/ti';
+import { IconContext } from "react-icons";
+
 export default function HeaderBlock() {
 
   const [scroll, setScroll] = useState(false);
 
-
-/*
-  useEffect(() => {}, []);
-  window.addEventListener("scroll", () => {
-    setScroll(window.scrollY > 10);
-  },[]);
-*/
   useEffect(() => {
 
     window.addEventListener("scroll", () => {
@@ -19,12 +15,18 @@ export default function HeaderBlock() {
     })
   }, [])
 
+  const hamburgerHandler = () => {
+    const el = document.getElementById('top-navbar');
 
-
-
+    if(el.className === 'sticky' || el.className === 'navbar') {
+      el.className += 'responsive';
+    } else {
+      el.className = 'navbar sticky'
+    }
+  }
 
   return (
-    <div className={scroll ? "sticky" : "navbar"} >
+    <div id='top-navbar' className={scroll ? "sticky" : "navbar"} >
       <nav>
         <ul>
 
@@ -48,6 +50,11 @@ export default function HeaderBlock() {
 
           <li className={scroll ? "nav-bar-dest-side" : "nav-bar-dest-top"}>
             <a href="#contact">CONTACT</a>
+          </li>
+          <li>
+            <IconContext.Provider value={{  className: "global-class-name", size: '2.5em'}}>
+              <a href='javascript:void(0);' className='hamburger' onClick={hamburgerHandler}><TiThMenuOutline /></a>
+            </IconContext.Provider>
           </li>
         </ul>
       </nav>
